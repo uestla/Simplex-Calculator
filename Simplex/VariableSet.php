3edfce82521ce3fea528f52335a30543594f18b6
Petr Kessler <kesspess@gmail.com>
2023-02-10 09:12:13 +0100

    updated phpDocs

--- Simplex/VariableSet.php
+++ Simplex/VariableSet.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the SimplexCalculator library
  *
- * Copyright (c) 2014 Petr Kessler (http://kesspess.1991.cz)
+ * Copyright (c) 2014 Petr Kessler (https://kesspess.cz)
  *
  * @license  MIT
  * @link     https://github.com/uestla/Simplex-Calculator
