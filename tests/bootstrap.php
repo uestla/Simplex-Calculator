7f27132ad50cae07bc8eadf92415fd74c42d5f4b
Petr Kessler <kesspess@gmail.com>
2023-03-20 12:00:03 +0100

    added CI layer

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Simplex\Tests;
+
+use Tester\Environment;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../Simplex/simplex.php';
+
+Environment::setup();
